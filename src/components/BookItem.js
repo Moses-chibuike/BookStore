fdaeeee5d616e19738cd4cae07bda8fed2439699
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2023-03-31 02:58:09 +0100

    set up routing for the App

---
+++ src/components/BookItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function BookItem({ book }) {
+  const { title, author } = book;
+  return (
+    <div>
+      <h2>
+        {title}
+      </h2>
+      <h2>
+        {author}
+      </h2>
+    </div>
+  );
+}
+
+BookItem.propTypes = {
+  book: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+
+export default BookItem;
